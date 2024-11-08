abb1bd8f0f2184ba12c5258d729299d5f3985c1f
Javier <padrongarciajavier04@gmail.com>
2024-11-08 20:45:09 +0000

    nuevo script para detectar papelera seleccionada

---
+++ recycle-ar/Assets/Scripts/TargetManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public enum Target{
+        plastico,
+        papel,
+        cristal,
+        organico
+    }   
+
+    public Target target;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
