6f5e19d32be3b6cd9ca6ac73422bf2a9105fe3b7
Javier <padrongarciajavier04@gmail.com>
2024-11-29 20:03:04 +0000

    Fixeos varios y version final 1

--- recycle-ar/Assets/Scripts/GuardarColeccionable.cs
+++ recycle-ar/Assets/Scripts/GuardarColeccionable.cs
@@ -1,21 +1,50 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GuardarColeccionable : MonoBehaviour
 {
     public string nombreColeccionable;
+    public TextMeshProUGUI textoEstado;
+    public Button botonRecolectar;
 
-    // Método que se llamará al presionar el botón
+    private ColeccionableManager manager;
+
+    private void Start()
+    {
+        manager = FindObjectOfType<ColeccionableManager>();
+
+        if (manager == null)
+        {
+            Debug.LogError("No se encontró el ColeccionableManager en la escena.");
+            return;
+        }
+
+        ActualizarUI();
+    }
+
     public void Guardar()
     {
-        ColeccionableManager manager = FindObjectOfType<ColeccionableManager>();
         if (manager != null)
         {
             manager.Recolectar(nombreColeccionable);
             Debug.Log($"Coleccionable '{nombreColeccionable}' guardado.");
+            ActualizarUI();
+        }
+    }
+
+    private void ActualizarUI()
+    {
+        if (manager.EsRecolectado(nombreColeccionable))
+        {
+            textoEstado.gameObject.SetActive(true);
+            botonRecolectar.gameObject.SetActive(false);
         }
         else
         {
-            Debug.LogError("No se encontró el ColeccionablesManager en la escena.");
+            textoEstado.gameObject.SetActive(false);
+            botonRecolectar.gameObject.SetActive(true);
         }
     }
 }
+
